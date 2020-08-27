63e4d18a148e7bb881c11e4c511a3bd8d4ed832d
mrbrunotte <mrbrunotte@gmail.com>
2020-08-27 13:43:59 +0200

    Add MoreTickets method

--- Excersice2LoopAndStringManipulation/Program.cs
+++ Excersice2LoopAndStringManipulation/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Excersice2LoopAndStringManipulation
 {
@@ -45,14 +49,79 @@ namespace Excersice2LoopAndStringManipulation
         }
         private static void TicketPrice()
         {
-            // Variable declaration
+            Console.WriteLine("##### Welcome to the Cineama! #####");
+            Console.WriteLine("1. Buy one single ticket.");
+            Console.WriteLine("2. Buy two or more tickets.");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    OneTicket();
+                    break;
+                case "2":
+                    MoreTickets();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void MoreTickets()
+        {
+            string strNumber, strTickets;
+            int age, tickets;
+            int i = 0;
+            int ticket = 0;
+            int youth = 0;
+            int adult = 0;
+            int senior = 0;
+            int totaltickets = 0;
+
+
+            Console.WriteLine("How many ticket's do you want to buy?");
+            strTickets = Console.ReadLine();
+            tickets = int.Parse(strTickets);
+
+            do
+            {
+                Console.Write("Please enter your age: ");
+                strNumber = Console.ReadLine();
+                age = int.Parse(strNumber);
+                Console.WriteLine("", i);
+
+                if (age < 20)
+                    youth += 80;
+                else if (age > 64)
+                    adult += 90;
+                else if (age > 20 && age < 64) 
+                    senior += 120;
+                else
+                    Console.WriteLine("Something went wrong! Did you really enter a digit number?");
+                
+                i++;
+            } while (i < tickets);
+
+            totaltickets = youth + adult + senior;
+            Console.WriteLine("You get a total of " + tickets + " tickets.");
+            Console.WriteLine("Your total ticket price is: " + totaltickets +"kr");
+            
+
+
+            
+
+            // if statement to give user correct ticket price
+            
+            //Menu();
+
+        }
+
+
+        private static void OneTicket()
+        {
             string number;
             int age;
-            // user enters age
-            // age is converted to int
-
-            Console.WriteLine("##### Welcome to the Cineama! #####");
-            Console.Write("1. Please enter you age: ");
+            int ticket;
+            Console.Write("Please enter your age: ");
             number = Console.ReadLine();
             age = int.Parse(number);
 
@@ -60,22 +129,23 @@ namespace Excersice2LoopAndStringManipulation
             if (age < 20)
             {
                 Console.WriteLine("\nYou are under 20 years old! \nYour ticket price is: 80 kr.");
-                Menu();
             }
             if (age > 64)
             {
                 Console.WriteLine("\nYou are a senior over 64 years! \nYour ticket price is: 90 kr.");
-                Menu();
+                
+            }
+            if (age > 20 && age < 64)
+            {
+                Console.WriteLine("\nSince you are not a senior or youth! \nYour ticket price is: 120 kr.");
+                
             }
             else
             {
-                Console.WriteLine("\nSince you are not a senior or youth! \nYour ticket price is: 120 kr.");
-                Menu();
-            }
+                Console.WriteLine("\nSomething went wrong! Did you really enter a digit number?");
                 
-
-
+            }
+            Menu();
         }
-
     }
 }
